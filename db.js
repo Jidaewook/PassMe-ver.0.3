b3ea151519a7bd0db851066a9cc9c216f7ec9e55
Jay <psatdoctor@naver.com>
2019-12-09 23:34:47 +0900

    Routes, Basic Setting
    기초설정 되었고, 몽고디비도 연결함.
    이제 모델링 하자.

---
+++ db.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('mongodb connected..'))
+    .catch(err => console.log(err));
