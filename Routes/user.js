b59ad86215b8161e5e4ce3603bdaee4149b9c9a0
Jay <psatdoctor@naver.com>
2019-12-22 10:03:52 +0900

    Forgot Password
    인증 이메일을 받고, 이메일에 링크 탈 주소가 들어가게 된다.

--- Routes/user.js
+++ Routes/user.js
@@ -94,4 +94,62 @@ router.post('/login', (req, res) => {
         })
 });
 
+// put = patch
+router.put('/forgot', (req, res) => {
+    const {email} = req.body;
+
+    userModel
+        .findOne({email}, (err, user) => {
+            if(err || !user){
+                return res.status(400).json({
+                    error: 'User with that email does not exist'
+                });
+            }
+            const token = jwt.sign(
+                {_id: user._id, name: user.name },
+                process.env.JWT_RESET_PASSWORD,
+                {expiresIn: '10m'}
+            )
+            const emailData = {
+                from: process.env.EMAIL_FROM,
+                to: email,
+                subject: `Password Reset Link`,
+                html: 
+                    `
+                        <h1>Please use the following link to reset your password</h1>
+                        <p>${process.env.CLIENT_URL}/auth/password/reset${token}</p>
+                        <hr />
+                        <p>This email may contain sensetive information</p>
+                        <p>${process.env.CLIENT_URL}</p>
+
+                    `
+            };
+            return user
+                .updateOne({ resetPasswordLink: token }, (err, success) => {
+                    if(err){
+                        return res.status(400).json({
+                            error: 'Database connection error on user password forgot request'
+                        });
+
+                    } else {
+                        sgMail
+                            .send(emailData)
+                            .then(setn => {
+                                return res.json({
+                                    message: `Email has been sent to ${email}. Follow the instruction to activate your account`
+                                });
+                            
+                            })
+                            .catch(err => {
+                                return res.json({
+                                    message: err.message
+                                });
+                            });
+                    };
+
+                })
+        })
+
+})
+
 module.exports = router;
