4ca3a3269604679e7df3a48e6d164b3aab72fe9b
Jay <psatdoctor@naver.com>
2019-12-22 11:28:16 +0900

    이메일 인증 수정
    수정판

--- Routes/user.js
+++ Routes/user.js
@@ -229,30 +229,62 @@ router.put('/reset', (req, res) => {
 });
 
 router.post('/account-activation', (req, res) => {
-    const {token} = req.body;
-    if(token) {
-        jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function(err, decoded){
-            if(err) {
+    // const {token} = req.body;
+    // if(token) {
+    //     jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function(err, decoded){
+    //         if(err) {
+    //             return res.status(401).json({
+    //                 error: 'Expired link. Signup again'
+    //             });
+    //         }
+    //         const {name, email, password} = jwt.decode(token);
+
+    //         const user = new userModel( name, email, password );
+
+    //         user
+    //             .save((err, user) => {
+    //                 if(err){ 
+    //                     return res.status(401).json({
+    //                         error: 'Error saving user in database. Try signup again'
+    //                     });
+    //                 } 
+    //                 return res.json({
+    //                     message: 'Signup success. Please Signin'
+    //                 });
+    //             });
+            
+    //     });
+    // } else {
+    //     return res.json({
+    //         message: 'Something went wrong. Try again.'
+    //     });
+    // }
+    const { token } = req.body;
+    if (token) {
+        jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function (err, decoded) {
+            if (err) {
+                console.log('JWT VERIFY IN ACCOUNT ACTIVATION ERROR', err);
                 return res.status(401).json({
                     error: 'Expired link. Signup again'
                 });
             }
-            const {name, email, password} = jwt.decode(token);
 
-            const user = new userModel( name, email, password );
+            const { name, email, password } = jwt.decode(token);
+
+            const user = new userModel({ name, email, password });
 
             user
                 .save((err, user) => {
-                    if(err){ 
+                    if (err) {
+                        console.log('SAVE USER IN ACCOUNT ACTIVATION ERROR', err);
                         return res.status(401).json({
                             error: 'Error saving user in database. Try signup again'
                         });
-                    } 
+                    }
                     return res.json({
-                        message: 'Signup success. Please Signin'
+                        message: 'Signup success. Please signin.'
                     });
                 });
-            
         });
     } else {
         return res.json({
