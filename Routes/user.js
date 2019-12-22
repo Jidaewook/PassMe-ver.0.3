10d671e396aeac0abd7f2a62c058a24098f9f118
Jay <psatdoctor@naver.com>
2019-12-22 10:16:56 +0900

    reset profile
    프로필(이름-닉네임, 패스워드) 수정과정
    userModel 안에 있는 newUser의 속성인 _id로 찾아서 그의 속성 중 name과  password를 고치고 updatedUser로 재저장을 하게 된다. 
    재저장을 하면서 hashed_password와 salt는 undefine으로 설정해 다른 사람이 볼 수 없게 한다.

--- Routes/user.js
+++ Routes/user.js
@@ -150,6 +150,51 @@ router.put('/forgot', (req, res) => {
                 })
         })
 
-})
+
+
+});
+
+router.put('/reset', (req, res) => {
+    const {name, password} = req.body;
+
+    userModel
+        .findOne({_id: req.user._id}, (err, user) => {
+            if(err || !user) {
+                return res.status(400).json({
+                    error: 'User not found'
+                });
+            } 
+            if(!name){
+                return res.status(400).json({
+                    error: 'Name is required'
+                });
+            } else {
+                user.name = name;
+            }
+            if(password) {
+                if(password.length < 6) {
+                    return res.status(400).json({
+                        error: 'Password should be min 6 characters long'
+                    });
+                } else {
+                    user.password = password;
+                }
+            }
+            user.save((err, updatedUser) => {
+                if(err) {
+                    console.log('USER UPDATE ERROR', err);
+                    return res.status(400).json({
+                        error: 'USER UPDATE FAILED'
+                    });
+                }
+                updatedUser.hashed_password = undefined;
+                updatedUser.salt = undefined;
+                res.json(updatedUser);
+            });
+
+        });
+});
+
+
 
 module.exports = router;
