da40ba271e9d871044da788686fe91622c706418
Jay <psatdoctor@naver.com>
2020-01-27 11:46:57 +0900

    Add detail bbs/category
    전체 게시물이 아닌, 게시판 내 게시물을 읽어오는 것과 게시판 내 게시물 하나를 읽어오는 것

--- Routes/bbs.js
+++ Routes/bbs.js
@@ -25,6 +25,50 @@ router.get('/', requireLogin, (req, res) => {
         });
 })
 
+
+//상세 데이터 불러오기(게시판 내 게시물 중에 하나만)
+router.get('/detail/:id', requireLogin, (req, res) => {
+    const id = req.params.id
+
+    bbsModel
+        .findById(id)
+        .then(result => {
+            res.status(200).json({
+                bbsInfo: result
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                msg: err.message
+            })
+        });
+});
+
+
+
+//게시판(카테고리) 불러오기
+router.get('/category/:catename', requireLogin, (req, res) => {
+    const catename = req.params.catename
+
+    bbsModel
+        //find는 복수 도큐멘츠를 찾아서 결과물로 가져오고, findOne은 하나의 도큐멘트를 찾아서 결과물로 가져온다.
+        .find({category: catename})
+        .then(result => {
+            res.status(200).json({
+                cateInfo: result
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                msg: err.message
+            })
+        });
+})
+
+
+
+
+
 //게시판 데이터 등록하기
 router.post('/write', requireLogin,(req, res) => {
     const {title, desc, category} = req.body;
